0e984fc12c06afcec4cf729ba7a834052c47ec6b
Teddy Fontaine <teddy.fontaine@circular.xyz>
2022-06-01 08:55:45 +0200

    feat: add docker-composee support

--- wcluster.sh
+++ wcluster.sh
@@ -1,10 +1,31 @@
 #!/usr/bin/env bash
 
-set -e
+source ./lib/lib.sh
 
-docker run -it --rm --privileged --cap-add=ALL -v /dev:/dev \
---mount type=bind,source="$(pwd)"/output,target=/app/output \
---mount type=bind,source="$(pwd)"/packer_cache,target=/app/packer_cache \
---mount type=bind,source="$(pwd)"/.ssh,target=/app/.ssh,readonly \
---mount type=bind,source="$(pwd)"/.kube,target=/app/.kube,readonly \
-raspberry-cluster-tool " $@ "
+COMPOSE_PROFILES=aws,tool
+
+compose="docker compose"
+
+if [[ $(command -v docker-compose) != "" ]]; then
+  echo -e "-> ${RED}docker-compose v1 detected, please use 'docker compose' commande insted of 'docker-compose'${NC}"
+  compose="docker-compose"
+  echo -e "$ sudo apt-get install docker-compose-plugin"
+fi
+
+if [[ $# == 0 ]]; then
+  services=$(${compose} ps --services)
+  echo ""
+  echo -e "-> ${L_BLUE}Cluster helper for Raspberry PI${NC}"
+  echo ""
+  echo "$0 <service_name> <options>"
+  echo ""
+  for service in ${services//$'\n'/' '}; do
+    echo -e "${ORANGE}$0 $service <options>${NC}"
+  done
+  echo ""
+  exit 1
+fi
+
+${compose} run --rm $@
+
+exit 0
